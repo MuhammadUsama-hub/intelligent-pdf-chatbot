7f10c01cdcc377ac25170e515d0fc76f0beb9251
Muhammad Usama <64538880+MuhammadUsama-hub@users.noreply.github.com>
2025-02-24 16:46:27 +0500

    integrate pinecone

--- server.ts
+++ server.ts
@@ -4,6 +4,38 @@ import http from "http";
 dotenv.config();
 
 import app from "@/app";
+import { Pinecone } from "@pinecone-database/pinecone";
+
+export const pc = new Pinecone({
+  apiKey: process.env.DATABASE_API_KEY!,
+});
+
+//PC index creation
+(async () => {
+  const indexName = "documents";
+  const existingIndexes = await pc.listIndexes();
+
+  if (
+    !existingIndexes.indexes?.map((index) => index.name).includes(indexName)
+  ) {
+    await pc.createIndex({
+      name: indexName,
+      dimension: 1536,
+      metric: "cosine",
+      spec: {
+        serverless: {
+          cloud: "aws",
+          region: "us-east-1",
+        },
+      },
+      deletionProtection: "disabled",
+      tags: { environment: "development" },
+    });
+    console.log(`Index ${indexName} created.`);
+  } else {
+    console.log(`Index ${indexName} already exists.`);
+  }
+})();
 
 const server = http.createServer(app);
 const PORT = process.env.PORT;
--- src/lib/open-ai/connect-ai.ts
+++ src/lib/open-ai/connect-ai.ts
@@ -1,6 +1,9 @@
 import OpenAI from "openai";
 
-const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || "" });
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "",
+  project: process.env.OPENAI_PROJECT_ID || "",
+});
 
 export default async function main(question: string) {
   const chatCompletion = await client.chat.completions.create({
